c99999bf96dfbd50a5382c9e86a22a566d85200c
gtlegend <yusuf_ar@live.com>
2023-09-16 17:42:01 -0400

    main/basic webpack implementation comepleted. Only styling required. Suspending until...

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,29 @@
-function createContact() {
-    const contact = document.createElement('div');
+const createContact = (() => {
+
+    
+
+    const contact = () => {
+        const main = document.getElementById('main');
+        
+        const contactHTML = `
+            <div class="contact" id="contact">
+                <p>Contact me NOW page.</p>
+            </div>
+        `;
+
+        main.innerHTML = contactHTML;
+
+    };
+    /* const contact = document.createElement('div');
     contact.classList.add('contactPage');
 
     const heroImg = new Image();
-    heroImg.src = 'assets/imgs/hero.jpg';
+    heroImg.src = 'assets/imgs/hero.jpg'; */
     /* home.appendChild(heroImg) */
 
-    contact.appendChild(createParagraph('contact me NOW page'));
+    /* contact.appendChild(createParagraph('contact me NOW page')); */
     return contact;
-};
+})();
 
 function createParagraph(text){
     const para = document.createElement('p');
